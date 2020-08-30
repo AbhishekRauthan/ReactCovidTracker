c295206175182d52d63906ad0e9dc36608b846ff
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-08-30 22:25:24 +0530

    Done initial setup; added Material-UI;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import "./App.css";
+import { Countries, CountriesResult } from "./interfaces/result.interface";
 
-function App() {
+const App: React.FC = () => {
+  const [countries, setCountries] = useState<Countries[]>([]);
+  const [country, setCountry] = useState("Worldwide");
+
+  useEffect(() => {
+    const getData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => {
+          return response.json();
+        })
+        .then((result) => {
+          const countries: Countries[] = result.map(
+            (country: CountriesResult) => {
+              return {
+                name: country.country,
+                value: country.countryInfo.iso2,
+              };
+            }
+          );
+
+          setCountries(countries);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app_dropbox">
+          <Select variant="outlined" value={country}>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country) => {
+              return <MenuItem value={country.value}>{country.name}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/interfaces/result.interface.ts
@@ -0,0 +1,11 @@
+export interface CountriesResult {
+  country: string,
+  countryInfo: {
+    iso2: string
+  }
+}
+
+export interface Countries {
+  name: string,
+  value: string;
+}
